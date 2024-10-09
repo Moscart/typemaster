671676b1adc7e22f3f01e95b14b0cba954f0b0a6
Moscart <danieltheo.73@gmail.com>
2024-10-09 14:53:48 +0700

    ✨feature: design for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,133 @@
 import Image from "next/image";
+import Logo from "@/public/assets/shared/logo.svg";
+import ICompatible from "@/public/assets/shared/icon-compatible.svg";
+import IBluetooth from "@/public/assets/shared/icon-bluetooth.svg";
+import IBattery from "@/public/assets/shared/icon-battery.svg";
+import ILight from "@/public/assets/shared/icon-light.svg";
+import ImageKeyboard from "@/public/assets/mobile/image-keyboard.jpg";
+import ImageGlass from "@/public/assets/mobile/image-glass-and-keyboard.jpg";
+import ImagePhone from "@/public/assets/mobile/image-phone-and-keyboard.jpg";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="">
+      <header className="px-6 pt-6 flex justify-between items-center">
+        <Image src={Logo} alt="Logo Typemaster" className="size-10" />
+        <Button className="uppercase bg-custom-light-gray text-custom-dark-gray font-bold text-[1rem] leading[1.625rem] h-auto px-[14px] py-3 rounded-[0.5rem] hover:bg-custom-dark-gray hover:text-white">
+          Pre-Order Now
+        </Button>
+      </header>
+      <main className="text-custom-dark-gray font-medium">
+        <section className="px-6 mt-[64px] flex flex-col gap-8">
+          <h1 className="font-black text-[3rem] leading-[3rem] uppercase">
+            Typemaster <span className="block">Keyboard</span>
+          </h1>
+          <p className="text-custom-gray leading[1.625rem]">
+            Improve your productivity and gaming without breaking the bank.
+            Upgrade to a high quality mechanical typing experience.
+          </p>
+          <div className="flex gap-8">
+            <Button className="uppercase bg-custom-orange text-white font-bold text-[1rem] leading-[1.625rem] h-auto px-[14px] py-3 rounded-[0.5rem] hover:bg-custom-light-orange">
+              Pre-Order Now
+            </Button>
+            <div className="uppercase text-custom-gray text-[1rem] leading-[1.625rem] font-bold items-center flex justify-center">
+              Release On 5/27
+            </div>
+          </div>
+        </section>
+        <section className="mt-16 ps-6">
+          <Image
+            src={ImageKeyboard}
+            alt="Logo Typemaster"
+            className="rounded-s-[1.25rem]"
+          />
+        </section>
+        <section className="pe-6 mt-6 flex gap-6">
+          <div className="relative rounded-e-[1.25rem] overflow-hidden z-10">
             <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={ImagePhone}
+              alt="Logo Typemaster"
+              className="h-full object-cover object-right mix-blend-multiply opacity-75"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+            <div className="bg-custom-orange w-full h-full absolute -z-10 top-0"></div>
+          </div>
+          <Image
+            src={ImageGlass}
+            alt="Logo Typemaster"
+            className="rounded-[1.25rem]"
+          />
+        </section>
+        <section className="px-6">
+          <div className="flex flex-col gap-6">
+            <h1 className="uppercase font-black text-[2rem] leading-[2.25rem] text-center mt-[75px]">
+              Mechanical <span className="block">Wireless</span>{" "}
+              <span className="block">Keyboard</span>
+            </h1>
+            <p className="leading-[1.625rem] text-custom-gray text-center">
+              The Typemaster keyboard boasts top-notch build and practical
+              design. It offers a wide variety of switches and keycaps, along
+              with reliable wireless connectivity.
+            </p>
+          </div>
+          <div className="flex flex-col gap-16 mt-[111px] mb-[96px]">
+            <div className="flex flex-col items-center text-center">
+              <div className="bg-custom-orange aspect-square w-[65px] rounded-2xl flex justify-center items-center">
+                <Image src={ICompatible} alt="Logo Typemaster" className="" />
+              </div>
+              <h2 className="uppercase font-black text-2xl leading-[1.75rem] mt-12">
+                Highly <span className="block">Compactible</span>
+              </h2>
+              <p className="mt-6 leading-[1.625rem] text-custom-gray">
+                Easy to use and works well with all major computer brands,
+                gaming consoles and mobile devices. Plug & play, no installation
+                or driver needed.
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <div className="bg-custom-orange aspect-square w-[65px] rounded-2xl flex justify-center items-center">
+                <Image src={IBluetooth} alt="Logo Typemaster" className="" />
+              </div>
+              <h2 className="uppercase font-black text-2xl leading-[1.75rem] mt-12">
+                Wireless <span className="block">With Bluetooth</span>
+              </h2>
+              <p className="mt-6 leading-[1.625rem] text-custom-gray">
+                Powerful 2.4G RF technology allows you to connect the cordless
+                keyboard from up to 30ft away. Simply plug the unifying receiver
+                into your computer.
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <div className="bg-custom-orange aspect-square w-[65px] rounded-2xl flex justify-center items-center">
+                <Image src={IBattery} alt="Logo Typemaster" className="" />
+              </div>
+              <h2 className="uppercase font-black text-2xl leading-[1.75rem] mt-12">
+                High Capacity <span className="block">Battery</span>
+              </h2>
+              <p className="mt-6 leading-[1.625rem] text-custom-gray">
+                Equipped with a long-lasting built-in battery, you’ll never have
+                to spend a dime on replaceable ones. Enjoy 40 hours of usage
+                time between charges.
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <div className="bg-custom-orange aspect-square w-[65px] rounded-2xl flex justify-center items-center">
+                <Image src={ILight} alt="Logo Typemaster" className="" />
+              </div>
+              <h2 className="uppercase font-black text-2xl leading-[1.75rem] mt-12">
+                RGB Backlit <span className="block">Modes</span>
+              </h2>
+              <p className="mt-6 leading-[1.625rem] text-custom-gray">
+                Choose from 4 backlight brightness levels and adjustable
+                breathing speed. Each key glows intensely in the dark and helps
+                you type in low light conditions.
+              </p>
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="leading-[1.625rem] text-custom-gray text-center pb-[50px]">
+        <span className="font-bold">Typemaster 2021</span> | All Rights Reserved
       </footer>
     </div>
   );
